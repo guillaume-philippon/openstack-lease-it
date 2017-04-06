f82d40c3388e433334de4b012a27690610db6095
Guillaume PHILIPPON <guillaume.philippon@olympe-hn.org>
2017-04-06 16:13:57 +0200

    Initial Import of OpenStack lease-it project

---
+++ openstack_lease_it/openstack_lease_it/config.py
@@ -0,0 +1,101 @@
+"""
+This module manage configuration file.
+* /etc/openstack-lease-it/config.ini
+* $HOME/.lease-it.ini
+"""
+import ConfigParser
+import os
+
+# List possible configuration file, last item has highest priority
+CONFIG_FILES = (
+    '/etc/openstack-lease-it/config.ini',
+    os.path.expanduser('~') + '/.lease-it.ini',
+)
+
+
+def read_configuration_file():
+    # Define default configuration, *NOT* all value have a default
+    global_configuration = {
+        # Django parameters
+        'DEBUG': False,
+
+        # OpenStack parameters
+        'OS_USERNAME': 'admin',
+        'OS_TENANT_NAME': 'admin',
+        'OS_PROJECT_NAME': 'admin',
+        'OS_IDENTITY_API_VERSION': '3',
+        'OS_USER_DOMAIN_NAME': 'default',
+        'OS_PROJECT_DOMAIN_NAME': 'default',
+    }
+    config = ConfigParser.RawConfigParser()
+
+    for file in CONFIG_FILES:
+        config.read(file)
+
+        try:
+            global_configuration['SECRET_KEY'] = config.get('django','secret_key')
+        except:
+            pass
+        try:
+            global_configuration['DEBUG'] = config.get('django', 'debug')
+        except:
+            pass
+
+        try:
+            global_configuration['OS_USERNAME'] = config.get('openstack', 'OS_USERNAME')
+        except:
+            pass
+        try:
+            global_configuration['OS_PASSWORD'] = config.get('openstack', 'OS_PASSWORD')
+        except:
+            pass
+        try:
+            global_configuration['OS_TENANT_NAME'] = config.get('openstack', 'OS_TENANT_NAME')
+        except:
+            pass
+        try:
+          global_configuration['OS_PROJECT_NAME'] = config.get('openstack', 'OS_PROJECT_NAME')
+        except:
+            pass
+        try:
+            global_configuration['OS_AUTH_URL'] = config.get('openstack', 'OS_AUTH_URL')
+        except:
+            pass
+        try:
+            global_configuration['OS_IDENTITY_API_VERSION'] = config.get('openstack', 'OS_IDENTITY_API_VERSION')
+        except:
+            pass
+        try:
+            global_configuration['OS_CACERT'] = config.get('openstack', 'OS_CACERT')
+        except:
+            pass
+        try:
+            global_configuration['OS_USER_DOMAIN_NAME'] = config.get('openstack', 'OS_USER_DOMAIN_NAME')
+        except:
+            pass
+        try:
+            global_configuration['OS_PROJECT_DOMAIN_NAME'] = config.get('openstack', 'OS_PROJECT_DOMAIN_NAME')
+        except:
+            pass
+        try:
+            global_configuration['AUTH_SERVER'] = config.get('auth','server')
+        except:
+            pass
+        try:
+            global_configuration['AUTH_USERNAME'] = config.get('auth','bind_dn')
+        except:
+            pass
+        try:
+            global_configuration['AUTH_PASSWORD'] = config.get('auth','password')
+        except:
+            pass
+        try:
+            global_configuration['AUTH_SEARCH'] = config.get('auth', 'search')
+        except:
+            pass
+        try:
+            global_configuration['AUTH_SEARCH_FILTER'] = config.get('auth', 'search_filter')
+        except:
+            pass
+
+    return global_configuration
---
+++ openstack_lease_it/openstack_lease_it/views.py
@@ -0,0 +1,145 @@
+#!/usr/local/bin/python2.7
+import json.tool
+from django.shortcuts import render, redirect
+from django.http import JsonResponse, HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import auth
+from openstack_lease_it.settings import GLOBAL_CONFIG
+from lease_it.Backend import OpenstackConnection
+
+
+@login_required()
+def hypervisor_dispatcher(request):
+    if 'format' in request.GET and request.GET['format'] == 'json':
+        return hypervisor_get_json(request)
+    else:
+        return hypervisor_get_html(request)
+
+
+@login_required()
+def hypervisor_get_html(request):
+    """
+
+    :param request:
+    :return:
+    """
+    return render(request, 'hypervisor_get_html.html')
+
+
+@login_required()
+def hypervisor_get_json(request):
+    """
+
+    :param request:
+    :return:
+    """
+    data = {}
+    list = []
+    con = OpenstackConnection(GLOBAL_CONFIG)
+    hypervisors = con.nova_get_hypervisors()
+
+    for each in hypervisors:
+        # print json.dumps(each._info, sort_keys=True, indent=4, separators=(',', ': '))
+        list.append({
+            'host': each._info['hypervisor_hostname'],
+            'id': each._info['id'],
+            'state': each._info['state'],
+            'status': each._info['status'],
+            'free_ram_mb': each._info['free_ram_mb'],
+            'free_disk_gb': each._info['free_disk_gb'],
+            'vcpus': each._info['vcpus'],
+            'vcpus_used': each._info['vcpus_used'],
+            'instances': each._info['running_vms'],
+        })
+
+    # flavors = con.nova_get_flavors()
+    # for each in flavors:
+    #     print json.dumps(each._info, sort_keys=True, indent=4, separators=(',', ': '))
+    data['list'] = list
+    return JsonResponse(data)
+
+
+@login_required()
+def home_dispatcher(request):
+    if 'format' in request.GET and request.GET['format'] == 'json':
+        return home_get_json(request)
+    else:
+        return home(request)
+
+
+@login_required()
+def home_get_json(request):
+    """
+
+    :param request:
+    :return:
+    """
+    data = {}
+    list = []
+    con = OpenstackConnection(GLOBAL_CONFIG)
+    # if 'test' in request.GET:
+    #     test = con.keystone_get_user()
+    #     print test
+
+    instances = con.nova_get_instances()
+
+    for each in instances:
+        # print json.dumps(each._info, sort_keys=True, indent=4, separators=(',', ': '))
+        list.append({
+            'name': each._info['name'],
+            'owner': each._info['user_id'],
+            'status': each._info['status'],
+            'project': each._info['tenant_id'],
+            'start': each._info['OS-SRV-USG:launched_at']
+        })
+    data['list'] = list
+    return JsonResponse(data)
+
+
+@login_required()
+def home(request):
+    """
+    :param request: HTTP request
+    :return: JSONResponse or HTTP
+    """
+    # con = OpenstackConnection(GLOBAL_CONFIG)
+    # print con.sess
+
+    # instances = con.nova_get_instances()
+    # for each in instances:
+        # print each._info
+        # print json.dumps(each._info, sort_keys=True, indent=4, separators=(',', ': '))
+        # print each._info['name']
+
+    return render(request, 'home_get_html.html')
+
+
+def login(request):
+    """
+    Standard login page for SLAM. If connected, it will redirect user to the page he request
+
+    :param request: HTTP request
+    :return: HTTP
+    """
+    redirect_page = request.GET.get('next', '/home')
+    if request.method == "POST":
+        user = auth.authenticate(username=request.POST['username'],
+                                 password=request.POST['password'])
+        if user is not None:
+            auth.login(request, user)
+            return HttpResponseRedirect(redirect_page)
+        else:
+            return render(request, "login.html")
+    else:
+        return render(request, "login.html")
+
+
+def logout(request):
+    """
+    Logout user and redirect to login page
+
+    :param request: HTTP request
+    :return: HTTP
+    """
+    auth.logout(request)
+    return redirect('/')
