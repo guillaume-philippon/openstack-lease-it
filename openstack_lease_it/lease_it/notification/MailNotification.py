080c77a2c61570c21ec79e7ff416719018c9eb58
Guillaume PHILIPPON <guillaume.philippon@olympe-hn.org>
2017-10-04 12:07:54 +0200

    Improve mail notification

--- openstack_lease_it/lease_it/notification/MailNotification.py
+++ openstack_lease_it/lease_it/notification/MailNotification.py
@@ -11,7 +11,7 @@ MAIL_CONTENT = {'delete': """
 Hi {0},
 
 Some of your Virtual Machine on Cloud will been removed due to expire lease time, you can update
-the lease on {1}:
+the lease on {1} :
 {2}
 
 --
@@ -35,9 +35,9 @@ class MailNotification(object):  # pylint: disable=too-few-public-methods
         Not yet implemented
         """
         self.users = users
-        # self.smtp = smtplib.SMTP_SSL(GLOBAL_CONFIG['NOTIFICATION_SMTP'])
-        # self.smtp.login(GLOBAL_CONFIG['NOTIFICATION_USERNAME'],
-        #                 GLOBAL_CONFIG['NOTIFICATION_PASSWORD'])
+        self.smtp = smtplib.SMTP_SSL(GLOBAL_CONFIG['NOTIFICATION_SMTP'])
+        self.smtp.login(GLOBAL_CONFIG['NOTIFICATION_USERNAME'],
+                        GLOBAL_CONFIG['NOTIFICATION_PASSWORD'])
 
     @staticmethod
     def format_user_instances(user):
@@ -46,49 +46,28 @@ class MailNotification(object):  # pylint: disable=too-few-public-methods
             text += "  - {name} started {created_at} and expire {lease_end} \n".format(**instance)
         return text
 
+    def format_mail(self, user, notification_type, instances):
+        try:
+            user_name = self.users[user]['name']
+        except KeyError:
+            user_name = 'Unknown User'
+        core_text = MAIL_CONTENT[notification_type]
+        instances_text = self.format_user_instances(instances)
+        return core_text.format(user_name,
+                                GLOBAL_CONFIG['NOTIFICATION_LINK'],
+                                instances_text)
+
     def send(self, notifications):
         for notification in notifications:
             for user in notifications[notification]:
-                instance_text = self.format_user_instances(notifications[notification][user])
+                mail_text = self.format_mail(user, notification, notifications[notification][user])
+                mail = MIMEText(mail_text)
+                mail['Subject'] = GLOBAL_CONFIG['NOTIFICATION_SUBJECT']
+                mail['From'] = GLOBAL_CONFIG['NOTIFICATION_EMAIL_HEADER']
                 try:
-                    user_name = self.users[user]['name']
+                    mail['To'] = self.users[user]['email']
                 except KeyError:
-                    user_name = "Unknowned User"
-                core_text = MAIL_CONTENT[notification]
-                print core_text.format(user_name,
-                                           GLOBAL_CONFIG['NOTIFICATION_LINK'],
-                                           instance_text)
-
-
-        # self.smtp.sendmail(GLOBAL_CONFIG['NOTIFICATION_EMAIL_HEADER'],
-        #                       [GLOBAL_CONFIG['NOTIFICATION_EMAIL_HEADER']],
-        #                       mail.as_string())
-        # notification.quit()
-
-
-# def format_mail(user, instances, notification_type):
-#     # TODO: Should be a dedicated Class
-#     subject = "Cloud@VD virtual machine notification"
-#     if notification_type == "delete":
-#         try:
-#             content = MAIL_CONTENT_DELETE.format(user['name'], instances)
-#         except KeyError:
-#             content = MAIL_CONTENT_DELETE.format('', instances)
-#     elif notification_type == "notify":
-#         try:
-#             content = MAIL_CONTENT_NOTIFY.format(user['name'], instances)
-#         except KeyError:
-#             content = MAIL_CONTENT_DELETE.format('', instances)
-#     mail = MIMEText(content)
-#     mail['Subject'] = subject
-#     mail['From'] = GLOBAL_CONFIG['NOTIFICATION_EMAIL_HEADER']
-#     mail['To'] = user['email']
-    # notification = smtplib.SMTP_SSL(GLOBAL_CONFIG['NOTIFICATION_SMTP'])
-    # notification.login(GLOBAL_CONFIG['NOTIFICATION_USERNAME'],
-    #                    GLOBAL_CONFIG['NOTIFICATION_PASSWORD'])
-    # notification.sendmail(GLOBAL_CONFIG['NOTIFICATION_EMAIL_HEADER'],
-    #                       [GLOBAL_CONFIG['NOTIFICATION_EMAIL_HEADER']],
-    #                       mail.as_string())
-    # notification.quit()
-    # print mail.as_string()
-
+                    mail['To'] = GLOBAL_CONFIG['NOTIFICATION_EMAIL_HEADER']
+                self.smtp.sendmail(GLOBAL_CONFIG['NOTIFICATION_EMAIL_HEADER'],
+                                   [GLOBAL_CONFIG['NOTIFICATION_EMAIL_HEADER']],
+                                   mail.as_string())
