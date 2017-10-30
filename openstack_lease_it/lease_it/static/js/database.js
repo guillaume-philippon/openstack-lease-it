02d3b6b3db295526c4c1b9c5ef7415a055a1cb35
Guillaume PHILIPPON <guillaume.philippon@olympe-hn.org>
2017-10-30 16:34:04 +0100

    Improve rendering for database admin table

--- openstack_lease_it/lease_it/static/js/database.js
+++ openstack_lease_it/lease_it/static/js/database.js
@@ -20,9 +20,16 @@ function buildDatabaseView(div_name) {
         pageLength: 25,
         columnDefs: [
             {
-                targets: [0, 1, 2],
+                targets: [0],
                 render: function ( data, type, row ) {
+                    var now = new Date()
+                    var lease_end_date = new Date(row.lease_end)
+                    if (lease_end_date < now) {
+                        return '<i class="material-icons red-text">delete_forever</i>' +
+                            formatText(data, MAX_STRING_LENGTH);
+                    } else {
                         return formatText(data, MAX_STRING_LENGTH);
+                    }
                 }
             }],
         drawCallback: function(settings, json) {
