7bd96e2fdd3c5944f1b6b99a4b68623335434002
Guillaume PHILIPPON <guillaume.philippon@olympe-hn.org>
2017-10-31 08:53:28 +0100

    Modify delete display

--- openstack_lease_it/lease_it/static/js/database.js
+++ openstack_lease_it/lease_it/static/js/database.js
@@ -1,5 +1,5 @@
 /*
-    buildInstancesView create a full display of Instance on div_name
+    buildDatabaseView create a full display of Da on div_name
 */
 function buildDatabaseView(div_name) {
     var table_columns = [
@@ -22,13 +22,13 @@ function buildDatabaseView(div_name) {
             {
                 targets: [0],
                 render: function ( data, type, row ) {
-                    var now = new Date()
-                    var heartbeat_date = new Date(row.heartbeat_at)
+                    var now = new Date();
+                    var heartbeat_date = new Date(row.heartbeat_at);
                     // If a VM as not been seen since 1 week, we can delete it
                     // 7 * 25 * 60 * 60 * 1000 = 604800000
                     if (heartbeat_date < now - 604800000) {
-                        return '<i class="material-icons red-text">delete_forever</i> ' +
-                            formatText(data, MAX_STRING_LENGTH);
+                        return buildDatabaseRowMenu(data) +
+                               formatText(data, MAX_STRING_LENGTH);
                     } else {
                         return formatText(data, MAX_STRING_LENGTH);
                     }
@@ -38,4 +38,37 @@ function buildDatabaseView(div_name) {
             $(".tooltipped").tooltip();
         },
     });
+}
+
+/*
+    buildDatabaseRowMenu build a menu for each row of Database Table
+*/
+function buildDatabaseRowMenu(data) {
+    menu = '<a class="btn-floating waves-effect waves-light tiny" onClick="swapDatabaseRowMenu(\'' + data + '\')">' +
+           '<i class="material-icons" id="database-icon-' + data + '">chevron_right</i></a> ' +
+           '<span hidden id="database-delete-' + data + '">' +
+           '<a class="btn-floating waves-effect waves-light red lighten-2"' +
+           'onClick="deleteDatabase(\'' + data + '\')">' +
+           '<i class="material-icons">delete</i></a></span> ';
+    return menu;
+}
+
+/*
+    swapDatabaseRowMenu swap on/off the delete button
+*/
+function swapDatabaseRowMenu(button) {
+    if ($('#database-delete-' + button).css('display') == 'none') {
+        $('#database-icon-' + button).text('chevron_left');
+    } else {
+        $('#database-icon-' + button).text('chevron_right');
+    }
+    $('#database-delete-' + button).toggle();
+}
+
+/*
+    deleteDatabase delete entry in database
+*/
+function deleteDatabase(entry) {
+    console.log('Entry: ' + entry + ' as been deleted');
+    swapDatabaseRowMenu(entry);
 }
